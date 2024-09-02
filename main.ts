2edcc314caa699317fe8ffbc42085a274537acfd
Daniel <rothenpielerdaniel@gmail.com>
2024-09-02 21:26:10 +0200

    initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,103 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Anthropic } from '@anthropic-ai/sdk';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface AnthropicPluginSettings {
+    apiKey: string;
+    activationHotkey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AnthropicPluginSettings = {
+    apiKey: '',
+    activationHotkey: 'Ctrl+Shift+A'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const INSTRUCTION_PREFIX = "You are a helpful AI assistant. Assist with the finishing of the following note. If the last sentence in the note is a specific instruction, follow that instruction, else simply fill out the remaining content such that it adheres to the title and general idea of the note. Try to keep the level of detail consistent with the note. If the level of detail is not discernable, assume that everything should be explained from the ground up and - apart from the most fundamental facts - be part of the note. Note, that when TeX code is required, use MathJax compatible notation. Inline TeX is done via ${content}$ while block TeX is done via $${conten}$$.";
+
+export default class AnthropicPlugin extends Plugin {
+    settings: AnthropicPluginSettings;
+    anthropic: Anthropic;
 
 	async onload() {
-		await this.loadSettings();
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        this.addSettingTab(new AnthropicSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+        this.registerCodeMirror((cm: CodeMirror.Editor) => {
+            cm.setOption("extraKeys", {
+                [this.settings.activationHotkey]: this.activateAnthropicAPI.bind(this)
+            });
+        });
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+            id: 'activate-anthropic-api',
+            name: 'Activate Anthropic API',
+            editorCallback: (editor: Editor) => this.activateAnthropicAPI(editor)
+        });
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+        this.anthropic = new Anthropic({ apiKey: this.settings.apiKey, dangerouslyAllowBrowser: true });
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+        this.anthropic = new Anthropic({ apiKey: this.settings.apiKey, dangerouslyAllowBrowser: true });
+    }
+	async activateAnthropicAPI(editor: Editor) {
+		const content = editor.getValue();
+
+		try {
+			const stream = await this.anthropic.completions.create({
+				model: 'claude-2',
+				max_tokens_to_sample: 1000,
+				prompt: `${INSTRUCTION_PREFIX}\n\nHuman: ${content}\n\nAssistant:`,
+				stream: true,
+			});
+
+			let response = '';
+			for await (const completion of stream) {
+				response += completion.completion;
+				editor.replaceSelection(completion.completion);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		} catch (error) {
+			new Notice('Error: ' + error.message);
+		}
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AnthropicSettingTab extends PluginSettingTab {
+    plugin: AnthropicPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: AnthropicPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const {containerEl} = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('API Key')
+            .setDesc('Enter your Anthropic API key')
+            .addText(text => text
+                .setPlaceholder('Enter your API key')
+                .setValue(this.plugin.settings.apiKey)
+                .onChange(async (value) => {
+                    this.plugin.settings.apiKey = value;
+                    await this.plugin.saveSettings();
+                }));
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Activation Hotkey')
+            .setDesc('Set the hotkey to activate the Anthropic API')
+            .addText(text => text
+                .setPlaceholder('Enter hotkey (e.g., Ctrl+Shift+A)')
+                .setValue(this.plugin.settings.activationHotkey)
+                .onChange(async (value) => {
+                    this.plugin.settings.activationHotkey = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
